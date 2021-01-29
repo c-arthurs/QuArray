f4b7b6268e2cc7ebf08beab6fb177511fa52fc67
Callum Arthurs <callumarthurs@yahoo.co.uk>
2021-01-29 10:38:01 +0000

    version update

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Callum Arthurs'
 author = 'Callum Arthurs'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5'
+release = '0.6'
 
 
 # -- General configuration ---------------------------------------------------
